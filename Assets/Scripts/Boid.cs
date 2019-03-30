f83721e4a391bbfd95b9fab9a1e8fca4d9accbcc
jokahata <jokahata@users.noreply.github.com>
2019-03-30 13:00:02 -0700

    Add simple collision avoidance

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class Boid : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 1;
+    private float speed = 2;
     private Rigidbody2D rigidbody2d;
+    private CollisionDetector collisionDetector;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Test");
         rigidbody2d = GetComponent<Rigidbody2D>();
+        collisionDetector = GetComponentInChildren<CollisionDetector>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Rotate the body
-        rigidbody2d.MoveRotation(rigidbody2d.rotation + 1);
+        Debug.Log("Debug says: " + collisionDetector.IsColliding);
+        if (collisionDetector.IsColliding)
+        {
+            rigidbody2d.MoveRotation(rigidbody2d.rotation + 1);
+        }
 
         // Move forward in direction currently facing
         rigidbody2d.MovePosition(rigidbody2d.transform.position + (transform.up * Time.deltaTime * speed));
---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+
+    public bool IsColliding
+    {
+        get; 
+        private set;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collision enter");
+        IsColliding = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        IsColliding = false;
+        Debug.Log("Collision exit");
+    }
+}
