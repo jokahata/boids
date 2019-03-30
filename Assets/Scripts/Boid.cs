3a0041ebb92ac6730606ad134a533616d3cff59b
jokahata <jokahata@users.noreply.github.com>
2019-03-30 11:28:13 -0700

    Make it 2D, add pyramid, simple movement

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    float speed = 1;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.MovePosition(new Vector3(rigidbody.position.x, rigidbody.position.y + Time.deltaTime * speed));
+        
+    }
+}
